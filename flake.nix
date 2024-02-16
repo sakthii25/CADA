2bf638f0b2ff3e6a0d00870a6e37362f7d2ea29a
eswar2001 <eswar.tadiparth@juspay.in>
2024-02-16 17:42:55 +0530

    fixed flake issues

--- flake.nix
+++ flake.nix
@@ -1,22 +1,41 @@
 {
   inputs = {
-    common.url = "github:nammayatri/common/ac4b0421da7088877deba240e0b19bf1f8616a86";
-    haskell-tools.url = "github:juspay/haskell-tools/de1208e5692004d62960dd30362b0fb430d8f1de";
-    haskell-tools.inputs.nixpkgs.follows = "common/nixpkgs";
+    classyplate.url = "github:Chaitanya-nair/classyplate/46f5e0e7073e1d047f70473bf3c75366a613bfeb";
+    classyplate.flake = false;
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    nixpkgs.url = "github:nixos/nixpkgs/43e3b6af08f29c4447a6073e3d5b86a4f45dd420";
+    systems.url = "github:nix-systems/default";
+    references.url = "github:eswar2001/references/35912f3cc72b67fa63a8d59d634401b79796469e";
+    references.flake = true;
+    haskell-tools.url = "github:eswar2001/haskell-tools/59e6168555d466237e27aabc794d5f4040b5ca8b";
   };
-  outputs = inputs:
-    inputs.common.lib.mkFlake { inherit inputs; } {
-      debug = true;
-      perSystem = { self', pkgs, lib, config, ... }: {
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake { inputs = inputs // { inherit (inputs) nixpkgs nixpkgs-latest; }; } {
+      systems = import inputs.systems;
+      imports = [inputs.haskell-flake.flakeModule];
+
+      perSystem = {
+        self',
+        pkgs,
+        lib,
+        config,
+        ...
+      }: {
         haskellProjects.default = {
-          devShell = {
-            tools = hp: { haskell-language-server = (pkgs.haskell.lib.compose.disableCabalFlag "fourmolu" pkgs.haskell.packages.ghc927.haskell-language-server).override { hls-fourmolu-plugin = null; }; };
+          basePackages = pkgs.haskell.packages.ghc8107;
+          packages = {
+            references.source = inputs.references;
+            classyplate.source = inputs.classyplate;
+            haskell-tools-parser.source = inputs.haskell-tools + /src/parser/haskell-tools-parser.cabal;
           };
-          imports = [inputs.haskell-tools.haskellFlakeProjectModules.output];
-          autoWire = [ "packages" "checks" ];
         };
-        process-compose = { };
-        packages.default = self'.packages.cada;
+        packages.default =  self'.packages.cada;
       };
     };
 }

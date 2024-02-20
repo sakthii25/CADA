0fb2f5526057c5c78b8d469603430b43ad8f4362
Praveen Kumar KN <praveen.kumar@juspay.in>
2024-02-20 10:49:58 +0530

    added-proper-function-signatures

--- src/Diff.hs
+++ src/Diff.hs
@@ -19,7 +19,7 @@ import Language.Haskell.Tools.AST.Ann
 import GHC.Hs.Extension
 import qualified Data.HashMap.Strict as HM
 
-extractModuleName :: String -> String
+extractModuleName :: FilePath -> String
 extractModuleName filePath =
     let (_, _, _, [moduleName]) = filePath =~ ".*src/(.*).hs" :: (String, String, String, [String])
     in map (\c -> if c == '/' then '.' else c) moduleName
@@ -58,12 +58,11 @@ run = do
             FDep.run
             cloneRepo repoUrl localRepoPath
             changedFiles <- getChangedFiles branchName currentCommit localRepoPath
-            let changedModules = map extractModuleName changedFiles
             print (show changedFiles)
             checkoutToBranch currentCommit
-            maybeCurrentAST  <- zip changedModules <$> getAstHashMap changedFiles localRepoPath
+            maybeCurrentAST   <- mkAst changedFiles localRepoPath
             checkoutToBranch branchName
-            maybePreviousAST  <- zip changedModules <$> getAstHashMap changedFiles localRepoPath
+            maybePreviousAST  <- mkAst changedFiles localRepoPath
             let listOfAstTuple = zip maybeCurrentAST maybePreviousAST
                 listOfFunMod   = map (\((moduleName, mCurrentAST), (_, mPreviousAST)) -> (moduleName, getAllFunctions mCurrentAST, getAllFunctions mPreviousAST)) listOfAstTuple
                 finalList      = map (\(moduleName, currentFns, previousFns) -> (moduleName, currentFns, previousFns, HM.keys $ HM.difference (HM.fromList currentFns) (HM.fromList previousFns))) listOfFunMod
@@ -73,8 +72,17 @@ run = do
         _ -> fail $ "can't proceed please pass all the arguments in the order of repoUrl localPath oldCommit newCommit but got: " <> show x
 
     where
-        processFile :: String -> FilePath -> IO (Maybe ((Ann AST.UModule (Dom GhcPs) SrcTemplateStage)))
-        processFile localRepoPath filePath = do
+        mkAst :: [FilePath] -> FilePath -> IO [(String, (Maybe (Ann AST.UModule (Dom GhcPs) SrcTemplateStage)))]
+        mkAst filePaths localRepoPath =
+            mapConcurrently (\f -> mkModuleNameAndAstTuple f localRepoPath) filePaths
+
+        mkModuleNameAndAstTuple :: FilePath -> FilePath -> IO (String, (Maybe (Ann AST.UModule (Dom GhcPs) SrcTemplateStage)))
+        mkModuleNameAndAstTuple filePath localRepoPath = do
+            ast <- processFile filePath localRepoPath
+            pure (extractModuleName filePath, ast)
+
+        processFile :: FilePath -> FilePath -> IO (Maybe ((Ann AST.UModule (Dom GhcPs) SrcTemplateStage)))
+        processFile filePath localRepoPath = do
             result <- try (moduleParser (localRepoPath <> "/src") (extractModuleName filePath)) :: IO (Either SomeException ((Ann AST.UModule (Dom GhcPs) SrcTemplateStage)))
             case result of
                 Right val -> pure $ Just val
@@ -82,11 +90,8 @@ run = do
                     print err
                     pure Nothing
 
-        getAstHashMap :: [FilePath] -> String -> IO [Maybe ((Ann AST.UModule (Dom GhcPs) SrcTemplateStage))]
-        getAstHashMap filePaths localRepoPath =
-            mapConcurrently (\file -> processFile localRepoPath file) filePaths
-
-        getFunctionModified (newFuns) oldFuns added moduleName = do
+        getFunctionModified :: (HM.HashMap String (Ann AST.UDecl (Dom GhcPs) SrcTemplateStage)) -> (HM.HashMap String (Ann AST.UDecl (Dom GhcPs) SrcTemplateStage)) -> [String] -> String -> FunctionModified
+        getFunctionModified newFuns oldFuns added moduleName = do
             let !y = HM.foldlWithKey (\acc@(FunctionModified dx mx ax _) k val ->
                         case HM.lookup k newFuns of
                             Just newVal -> if (val == newVal) then acc else FunctionModified dx (k : mx) ax moduleName
